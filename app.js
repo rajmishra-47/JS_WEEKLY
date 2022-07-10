2fd4ff8c8f910a91e93e238c9adb78426e8fbb3e
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-10 13:24:18 +0530

    first

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 'use strict'
 
-let a=1;
+// let a=5;
 
-console.log(a);
+// console.log(a);
+
+// //loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i+" "+ (i+1));
+// }
+
+// const b=()=>{
+
+//     let a=10;
+
+//     return a;
+// }
+
+// console.log(b());
+
