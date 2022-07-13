fab2f2b24a6a1f17c87ab199fb99438b2b7ef85a
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-13 22:07:16 +0530

    for each

--- d4/app.js
+++ d4/app.js
@@ -1,9 +1,16 @@
 //Arrays
 
-let a=[1,2,3,4];
+// let a=[1,2,3,4];
 
-console.log(a);
-console.log(a[0]);
-console.log(a[a.length-1]);
-console.log(a[a.length/2]);
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[a.length-1]);
+// console.log(a[a.length/2]);
+
+let a=[1,2,3,4,5,6,7,8,9,0];
+
+a.forEach(b=>{
+
+    console.log(b);
+})
 
