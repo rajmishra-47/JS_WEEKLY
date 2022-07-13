5c72b49d1bd214a416db0de81392ed2c7925291a
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-13 22:08:21 +0530

    includes methord

--- d4/app.js
+++ d4/app.js
@@ -7,10 +7,14 @@
 // console.log(a[a.length-1]);
 // console.log(a[a.length/2]);
 
+// let a=[1,2,3,4,5,6,7,8,9,0];
+
+// a.forEach(b=>{
+
+//     console.log(b);
+// })
+
 let a=[1,2,3,4,5,6,7,8,9,0];
 
-a.forEach(b=>{
-
-    console.log(b);
-})
-
+console.log(a.includes(0));
+console.log(a.includes(-1));
