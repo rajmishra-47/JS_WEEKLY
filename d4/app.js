26b17a0ef632f2db244262efa48cb2072e6d1b18
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-13 22:24:36 +0530

    Filter

--- d4/app.js
+++ d4/app.js
@@ -14,7 +14,13 @@
 //     console.log(b);
 // })
 
-let a=[1,2,3,4,5,6,7,8,9,0];
+// let a=[1,2,3,4,5,6,7,8,9,0];
 
-console.log(a.includes(0));
-console.log(a.includes(-1));
+// console.log(a.includes(0));
+// console.log(a.includes(-1));
+
+// let a=[1,2,3,4,5,6,7,8,9,0];
+
+// const c=a.filter(b=>b>=3)
+
+// console.log(c);
