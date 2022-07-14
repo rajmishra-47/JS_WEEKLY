8248b19ea14c2da0bbb26aeda9651cfdcf4c1b6e
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-15 02:22:56 +0530

    ES6 strings

---
+++ d5/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//string ES6
+
+let a="Raj";
+
+console.log(`${a}`);
+//case sensetive;
+console.log(a.startsWith('R'));
+console.log(a.startsWith('a'));
+console.log(a.endsWith('j'));
