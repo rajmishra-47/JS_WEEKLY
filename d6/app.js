209fa1def1ce94eda1e19c97c8faadd74c534124
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-15 21:27:57 +0530

    default parameters

--- d6/app.js
+++ d6/app.js
@@ -2,21 +2,29 @@
 
 //object destructuring
 
-let a={
-    name:"raj",
-    age:20,
-    degree:"Btech",
-    hobby:{
-        first:"games",
-        second:"food",
-    }
+// let a={
+//     name:"raj",
+//     age:20,
+//     degree:"Btech",
+//     hobby:{
+//         first:"games",
+//         second:"food",
+//     }
+// }
+
+// // console.log(a);
+// console.log(`hello my name is ${a.name} and my age is ${a.age}`);
+
+// let name,age,degree,hobby;
+
+// ({name,age,degree,hobby}=a);
+
+// console.log(`hello my name is ${name} and my age is ${age} and ${hobby.first}`);
+
+
+//Default parameters
+function add(a,b=1){
+    return a+b;
 }
 
-// console.log(a);
-console.log(`hello my name is ${a.name} and my age is ${a.age}`);
-
-let name,age,degree,hobby;
-
-({name,age,degree,hobby}=a);
-
-console.log(`hello my name is ${name} and my age is ${age} and ${hobby.first}`);
+console.log(add(2));
