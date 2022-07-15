5ebd9269cbd374d1cc48392c71aabe5696908f55
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-15 17:18:45 +0530

    obj destructuring

--- d6/app.js
+++ d6/app.js
@@ -2,11 +2,21 @@
 
 //object destructuring
 
-const a={
+let a={
     name:"raj",
     age:20,
     degree:"Btech",
+    hobby:{
+        first:"games",
+        second:"food",
+    }
 }
 
-console.log(a);
+// console.log(a);
 console.log(`hello my name is ${a.name} and my age is ${a.age}`);
+
+let name,age,degree,hobby;
+
+({name,age,degree,hobby}=a);
+
+console.log(`hello my name is ${name} and my age is ${age} and ${hobby.first}`);
